e2a433c63826123d3d794800e14ee040f009a130
asihrio11 <ashirio0609@gmail.com>
2025-08-03 14:53:33 +0900

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.scss';
+import type { Metadata } from 'next';
 
-export const metadata = {
-  title: '商品＆女優検索',
-  description: 'FANZA検索ツール',
+export const metadata: Metadata = {
+  title: 'AV女優・動画検索 | あなた好みの厳選作品を発見',
+  description: 'AV女優や動画を、あなたの好みに合わせて厳選。検索・絞り込みも簡単。最速でお気に入り作品に出会える！',
 };
 
 export default function RootLayout({
@@ -12,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
