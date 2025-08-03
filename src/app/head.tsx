f9a485808eb13db80f526298fb6435f91564aac0
asihrio11 <ashirio0609@gmail.com>
2025-08-03 14:40:14 +0900

    SEO対策

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>AV女優・動画検索 | あなたの好みに合わせた厳選作品</title>
+      <meta
+        name="description"
+        content="AV女優やキーワード、カップ数などからFANZA動画を絞り込み検索。最速30秒でお気に入りの作品が見つかります。"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="UTF-8" />
+    </>
+  );
+}
