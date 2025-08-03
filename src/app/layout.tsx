e60fe7aa453ca8f3ea69ef38943870108dd6882f
asihrio11 <ashirio0609@gmail.com>
2025-08-03 15:11:11 +0900

    OGP修正したよ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import Head from 'next/head';
+import '../styles/globals.scss';
+import type { Metadata } from 'next';
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'AV女優・動画検索 | あなた好みの厳選作品を発見',
+  description: 'AV女優や動画を、あなたの好みに合わせて厳選。検索・絞り込みも簡単。最速でお気に入り作品に出会える！',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <>
-      <Head>
-        <title>AV女優・動画検索 | あなた好みの厳選作品を発見</title>
-        <meta name="description" content="AV女優や動画を、あなたの好みに合わせて厳選。検索・絞り込みも簡単。最速でお気に入り作品に出会える！" />
-
-        {/* OGPタグ */}
-        <meta property="og:title" content="AV女優・動画検索 | あなた好みの厳選作品を発見" />
-        <meta property="og:description" content="あなたの好みに合わせたAV女優・動画を検索できるサービスです。" />
-        <meta property="og:image" content="https://yourdomain.com/ogp.jpg" />
-        <meta property="og:url" content="https://adult-serch-hnsz.vercel.app/" />
-        <meta property="og:type" content="website" />
-      </Head>
-      {/* コンテンツ */}
-    </>
+    <html lang="ja">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
   );
 }
