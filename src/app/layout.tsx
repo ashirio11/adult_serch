7c9ac4f3c212ff246703992dccb860b16c6381ee
asihrio11 <ashirio0609@gmail.com>
2025-08-05 23:53:32 +0900

    ✅ Add updated Google site verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,3 @@
-import '../styles/globals.scss';
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'AV女優・動画検索 | あなた好みの厳選作品を発見',
-  description: 'AV女優や動画を、あなたの好みに合わせて厳選。検索・絞り込みも簡単。最速でお気に入り作品に出会える！',
-};
-
 export default function RootLayout({
   children,
 }: {
@@ -14,6 +6,14 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <title>AV女優・動画検索 | あなた好みの厳選作品を発見</title>
+        <meta
+          name="description"
+          content="AV女優や動画を、あなたの好みに合わせて厳選。検索・絞り込みも簡単。最速でお気に入り作品に出会える！"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta name="google-site-verification" content="4gUtlL7KwbUJHn8PUnCLWin8_P86TfkWOTHGUcEyGV0" />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>{children}</body>
